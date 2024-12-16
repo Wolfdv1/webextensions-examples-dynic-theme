ae8c30e7153924747e3a1a895ffc7cb8077c50c6
Wolfdv1 <dan@danw.pl>
2024-12-16 13:05:09 +0100

    Update background.js

--- dynamic-theme/background.js
+++ dynamic-theme/background.js
@@ -46,4 +46,4 @@ checkTime();
 
 // Set up an alarm to check this regularly.
 browser.alarms.onAlarm.addListener(checkTime);
-browser.alarms.create('checkTime', {periodInMinutes: 5});
+browser.alarms.create('checkTime', {periodInMinutes: 1});
